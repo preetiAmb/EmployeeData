aebb5083333b1fb71a59e0ca9000f2f052b68303
Preeti Ambekar <preeti.ambekar@quickchannel.com>
2021-05-30 23:12:43 +0200

    Added employeeList component to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import EmployeesList from './components/employeesList';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import EmployeesList from "./components/employeesList";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <EmployeesList/>
+    <div className='App'>
+      <EmployeesList />
     </div>
   );
-}
+};
 
 export default App;
