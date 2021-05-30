2233f6ff96ae3038e57cdf312e4414032ad7fbc4
Preeti Ambekar <preeti.ambekar@quickchannel.com>
2021-05-30 23:09:30 +0200

    feat/search and getData

--- src/components/employeesList.js
+++ src/components/employeesList.js
@@ -5,109 +5,169 @@ import {
   Row,
   Col,
   Form,
-  Button,
+  InputGroup,
+  FormControl,
 } from "react-bootstrap";
 import "./employeesList.css";
-import { RiLinkedinBoxFill } from "react-icons/ri";
-import { AiFillTwitterCircle } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+import { RiTwitterFill } from "react-icons/ri";
 import { AiFillGithub } from "react-icons/ai";
 import { MdPlace } from "react-icons/md";
+import { ImSearch } from "react-icons/im";
+import axios from "axios";
+import Scroll from "./scroll";
 
 const EmployeesList = () => {
   const [employeeData, setemployeeData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  const [filteredEmployees, setFilteredEmployees] = useState([]);
 
   useEffect(() => {
-    fetch(`https://api.tretton37.com/ninjas`, {
-      mode: "cors",
-      "Access-Control-Allow-Headers": "*",
-      "Access-Control-Allow-Methods": "*",
-    })
-      .then((res) => res.json())
-      .then((res) => setemployeeData(res));
-    /* .then((data) => data.json())
-      .then((resp) => {
-        if(resp.success) {
-          setemployeeData(resp);
-        } else {
-          console.log(resp);
-        }
-        {resp.map(e => console.log(e.name))}
-    
-      })  */
-  });
+    setLoading(true);
+    axios
+      .get("https://api.tretton37.com/ninjas", {
+        mode: "cors",
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Methods": "*",
+      })
+      .then((res) => {
+        setemployeeData(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    setFilteredEmployees(
+      employeeData.filter((employee) => {
+        return Object.values(employee)
+          .join(" ")
+          .toLowerCase()
+          .includes(search.toLowerCase());
+      })
+    );
+  }, [search, employeeData]);
+
+  if (loading) {
+    return <p>Loading employeeData...</p>;
+  }
 
   return (
     <Container w-100>
-      <h1 className="mt-5 align-items-center">tretton37</h1>
-      <Form className="mb-5 mt-5">
-        <Form.Row className="align-items-center">
-          <Col xs="8" className="my-1 align-items-center">
-            <Form.Control
-              as="select"
-              className="mr-sm-2"
-              id="inlineFormCustomSelect"
-              custom
-            >
-              <option value="0">Choose...</option>
-              <option value="1">Name</option>
-              <option value="2">Office</option>
-            </Form.Control>
-          </Col>
-        </Form.Row>
+      <h1 className='mt-5 col-12'>tretton37</h1>
+      <Form inline className='pb-2 pt-3 justify-content-center'>
+        <InputGroup className='speaker-search col-12'>
+          <InputGroup.Prepend className='icon-button'>
+            <InputGroup.Text>
+              <ImSearch />
+            </InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            className='form-control col-6'
+            type='text'
+            placeholder='Search'
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </InputGroup>
       </Form>
-      <Row xs="12" className="justify-content-md-center">
-        <Col className="card-columns mb-5 image-flip">
-          <div className="justify-content-center">
-          {employeeData.length &&
-            employeeData.map((employee, name) => {
-              return (
-                <>
-                <Row>
-                  <Col xs="12">
-                  <Col xs="4">
-                  <Card.Img
-                    variant="top"
-                    alt="img"
-                    width={50}
-                    height={100}
-                    key={employee.name}
-                    src={employee.imagePortraitUrl}
-                  />
-                  </Col>
-                  <Col xs="8">
-                  <Row xs="12">
-                    <Col xs="6">
-                      <h5 aria-label={employee.name}>{employee.name}</h5>
-                    </Col>
-                    <Col xs="1">
-                      <Button variant="light" href={"https://www.linkedin.com"+employee.linkedIn} target="_blank">
-                        <RiLinkedinBoxFill />
-                      </Button>
-                    </Col>
-                    <Col xs="1">
-                      <Button variant="light" href={"https://github.com/"+employee.gitHub} target="_blank">
-                        {" "}
-                        <AiFillGithub />
-                      </Button>
-                    </Col>
-                    <Col xs="1">
-                      <Button variant="light" href={"https://twitter.com/"+employee.twitter} target="_blank">
-                        <AiFillTwitterCircle />
-                      </Button>
-                    </Col>
-                  </Row>
-                  <h6 aria-label={employee.office}>
-                    <MdPlace /> {employee.office}
-                  </h6>
-                  </Col>
-                  </Col>
-                  </Row>
-                </>             
-              );
-            })}
-            
+
+      <Row xs='12' className='justify-content-md-center'>
+        <Scroll>
+          <Col className='card-columns mb-5 pt-3'>
+            <div className='card-list justify-content-center'>
+              {filteredEmployees.length > 0 ? (
+                filteredEmployees.map((employee, idx) => {
+                  return (
+                    <>
+                      <Row>
+                        <Col xs='12'>
+                          <Card key={idx}>
+                            <Row>
+                              <Col xs='6'>
+                                <Card.Img
+                                  variant='top'
+                                  className='p-2'
+                                  alt='img'
+                                  width={100}
+                                  height={200}
+                                  key={employee.idx}
+                                  src={employee.imagePortraitUrl}
+                                />
+                              </Col>
+                              <Col xs='6'>
+                                <h1
+                                  className='h6 mt-5'
+                                  aria-label={employee.name}
+                                >
+                                  {employee.name}
+                                </h1>
+                                <p aria-label={employee.office}>
+                                  <MdPlace /> {employee.office}
+                                </p>
+
+                                <Row className='p-1'>
+                                  <Col xs='1'>
+                                    <a
+                                      className='icon'
+                                      aria-label='Linkedin'
+                                      variant='light'
+                                      href={
+                                        "https://www.linkedin.com" +
+                                        employee.linkedIn
+                                      }
+                                      target='_blank'
+                                      rel='noreferrer'
+                                    >
+                                      <AiFillLinkedin />
+                                    </a>
+                                  </Col>
+                                  <Col xs='1'>
+                                    <a
+                                      className='icon'
+                                      aria-label='github'
+                                      variant='light'
+                                      href={
+                                        "https://github.com/" + employee.gitHub
+                                      }
+                                      target='_blank'
+                                      rel='noreferrer'
+                                    >
+                                      <AiFillGithub />
+                                    </a>
+                                  </Col>
+                                  <Col xs='1'>
+                                    <a
+                                      className='icon'
+                                      variant='light'
+                                      aria-label='twitter'
+                                      href={
+                                        "https://twitter.com/" +
+                                        employee.twitter
+                                      }
+                                      target='_blank'
+                                      rel='noreferrer'
+                                    >
+                                      <RiTwitterFill />
+                                    </a>
+                                  </Col>
+                                </Row>
+                              </Col>
+                            </Row>
+                          </Card>
+                        </Col>
+                      </Row>
+                    </>
+                  );
+                })
+              ) : (
+                <p>No employeeData found...</p>
+              )}
             </div>
-        </Col>
+          </Col>
+        </Scroll>
       </Row>
     </Container>
   );
